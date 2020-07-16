a379d9147d72c6c18af8714f71580ef32a2b383c
ahmedashraf96 <ahmedashrafelnaqeeb@gmail.com>
2020-07-16 15:52:05 +0200

    Testing code uploaded

---
+++ Project_Testing_Code/ATtiny4 code/ATtiny4 code/Release/main.d
@@ -0,0 +1,21 @@
+main.d main.o: .././main.c .././ATtiny4.h \
+ e:\atmel\ studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
+ e:\atmel\ studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ e:\atmel\ studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stdint.h \
+ e:\atmel\ studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ e:\atmel\ studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
+ e:\atmel\ studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h
+
+.././ATtiny4.h:
+
+e:\atmel\ studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
+
+e:\atmel\ studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+e:\atmel\ studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\4.9.2\include\stdint.h:
+
+e:\atmel\ studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+e:\atmel\ studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
+
+e:\atmel\ studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
---
+++ Project_Testing_Code/ATtiny4 code/ATtiny4 code/main.c
@@ -0,0 +1,53 @@
+/**
+ * @file 	main.c
+ * @author 	Ahmed Ashraf (ahmedashrafelnaqeeb@gmail.com)
+ * @brief   This file contains the starting point (main function) of the power manager application
+ * @version 1.0
+ * @date 	2020-07-12
+ * 
+ * @copyright Copyright (c) 2020
+ * 
+ */
+
+
+/************************************************************************/
+/*                         Includes                                     */
+/************************************************************************/
+
+#include "ATtiny4.h"
+
+#define  F_CPU 8000000UL
+#include "util/delay.h"
+/************************************************************************/
+/*            Application entry point & testing function                */
+/************************************************************************/
+
+void testApp(void)
+{
+	SET_BIT(DDRB , DDRB_PB3);
+	
+	while(1)
+	{
+		SET_BIT(PORTB , PORTB_PB3);
+		_delay_ms(1000);
+		
+		CLEAR_BIT(PORTB , PORTB_PB3);
+		_delay_ms(1000);
+	}
+	return;
+}
+
+/**
+ * @brief  This the entry point of the power manager application
+ * 
+ * @return int 0 if everything is good and another value if there's an error 
+ */
+int main(void)
+{
+	testApp();
+	
+	while(1)
+	{
+	}
+	return 0;
+}
